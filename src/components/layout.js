10c232abcb8cefc4b02d858030b2d7a20ef589ac
Ying Wang <yingw787.personal@gmail.com>
2020-03-21 14:30:47 -0400

    Update terms.js layout.js.

--- src/components/layout.js
+++ src/components/layout.js
@@ -68,7 +68,7 @@ const Layout = ({children}) => (
                                     <ul>
                                         <li><Link to="/contact" title={"Contact Us"}>Contact</Link></li>
                                         <li><Link to="/privacy" title={"Privacy Policy"}>Privacy</Link></li>
-                                        <li><Link to="/terms" title={"Terms Of Use"}>Terms Of Use</Link></li>
+                                        <li><Link to="/terms" title={"License"}>License</Link></li>
                                     </ul>
                                 </div>
                             </div>
