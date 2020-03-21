27e910321086d92781aab725280e30e1947107a7
Ying Wang <yingw787.personal@gmail.com>
2020-03-21 16:00:42 -0400

    Update contact.js.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,13 +14,13 @@ const ContactPage = () => (
 
                 <div className={"row"}>
                     <div className={"col-7"}>
-                        <p>We take our commitment to our users seriously. If you need our help with your user account, have questions about how to use the HiStaff, any feedback or are experiencing technical difficulties, please do not hesitate to contact us.</p>
+                        <p>If you have any questions, comments, or concerns, please do not hesitate to contact us.</p>
                     </div>
 
                     <div className={"col-5"}>
                         <div className={"contact-items"}>
                             <p>Plain email is the best way to contact us.</p>
-                            <h2>contact@histaff.io</h2>
+                            <h2>me@yingw787.com</h2>
                         </div>
                     </div>
                 </div>
